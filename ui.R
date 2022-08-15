db79d7dfedf0e649dabc46ee3ad04a8cffcb28c3
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-08-15 17:20:18 +0100

    renamed some outputIDs

--- ui.R
+++ ui.R
@@ -17,13 +17,13 @@ ui <- navbarPage(
            fluidRow(
              # Add conditionalPanel, if council input is blank show Scotland map - outputID = scot_map_tab_1
              # This will include absolute panel with across areas graph and within areas graph;
-             # across areas graph - outputID = across_areas_plt_tab_1
-             # within areas graph - outputID = within_areas_plt_tab_1
+             # across areas graph - outputID = across_areas_plot_tab_1
+             # within areas graph - outputID = within_areas_plot_tab_1
             
              # Add 2nd conditionalPanel, if council input is not blank show council map - outputID = la_map_tab_1
              # This will include absolute panel with across areas graph and within areas graph;
-             # across areas graph - outputID = across_areas_plt_tab_1
-             # within areas graph - outputID = within_areas_plt_tab_1
+             # across areas graph - outputID = across_areas_plot_tab_1
+             # within areas graph - outputID = within_areas_plot_tab_1
            )),
 
 # Similar Areas Tab (Tab 2) --------------------------------------------------------------------
@@ -32,7 +32,7 @@ ui <- navbarPage(
            fluidRow(
              # Add selectize input for council dropdown - inputID = la_choice_tab_2
              column(3),
-             # Add UiOutput for small area dropdown - outputID = small_area_choice_tab_2
+             # Add UiOutput for small area dropdown - outputID = small_area_output_tab_2
              column(3),
              # Add selectize input for year dropdown - inputID = year_choice_tab_2
              column(3),
@@ -42,12 +42,12 @@ ui <- navbarPage(
            fluidRow(
              # Add conditionalPanel, if council and small area input are blank show Scotland map - outputID = scot_map_tab_2
              # This will include absolute panel with across areas graph and similar areas graph;
-             # across areas graph - outputID = across_areas_plt_tab_2
-             # similar areas graph - outputID = similar_areas_plt_tab_2
+             # across areas graph - outputID = across_areas_plot_tab_2
+             # similar areas graph - outputID = similar_areas_plot_tab_2
              
              # Add 2nd conditionalPanel, if council and small area input are not blank show council map - outputID = la_map_tab_2
              # This will include absolute panel with across areas graph and similar areas graph;
-             # across areas graph - outputID = across_areas_plt_tab_2
-             # similar areas graph - outputID = similar_areas_plt_tab_2
+             # across areas graph - outputID = across_areas_plot_tab_2
+             # similar areas graph - outputID = similar_areas_plot_tab_2
            ))
 )
