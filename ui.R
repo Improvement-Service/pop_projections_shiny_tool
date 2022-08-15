4dce829244c3831337e873c581b2c7cfe23c1a13
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-08-15 15:31:08 +0100

    Set out basic Ui structure

--- ui.R
+++ ui.R
@@ -1,5 +1,20 @@
 ui <- navbarPage(
   title = "Small Area Population Projections",
-  tabPanel("Population size"),
-  tabPanel("Comparison of similar areas")
+  tabPanel("Population size", 
+           fluidRow(
+             column(3),
+             column(3),
+             column(3),
+             column(3)
+           ),
+           fluidRow()),
+  tabPanel("Comparison of similar areas",
+           fluidRow(
+             column(3),
+             column(3),
+             column(3),
+             column(3)
+           ),
+           fluidRow()
+           )
 )
