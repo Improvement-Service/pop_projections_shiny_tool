da7ec977a13c161bd56f88db3281ea3147fa3f41
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-08-16 10:55:35 +0100

    Read in population projection data and added Scotland level data to the data frame

--- global.R
+++ global.R
@@ -1,1 +1,19 @@
 library(shiny)
+library(tidyverse)
+library(readxl)
+
+projection_data <- read.csv("Data files/All Councils - Detailed Projections - Male & Female.csv")
+
+# Add Scotland level data to projection_data
+
+la_data <- projection_data %>% filter(Level == "Council")
+
+scot_data <- la_data %>% 
+  group_by(Year, Sex, Age) %>% 
+  summarise(Population = sum(Population)) %>%
+  mutate(Council.Name = "Scotland") %>%
+  mutate(Level = "Scotland") %>%
+  mutate(Area.Name = "Scotland") %>%
+  select(Council.Name, Level, Area.Name, Year, Sex, Age, Population)
+
+projection_data <- rbind(projection_data, scot_data)
