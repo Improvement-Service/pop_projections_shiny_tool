774fd87bb9f013b064b02bf3050772e6a33f647d
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-08-16 11:04:33 +0100

    Added persons data to the projections data frame

--- global.R
+++ global.R
@@ -5,7 +5,6 @@ library(readxl)
 projection_data <- read.csv("Data files/All Councils - Detailed Projections - Male & Female.csv")
 
 # Add Scotland level data to projection_data
-
 la_data <- projection_data %>% filter(Level == "Council")
 
 scot_data <- la_data %>% 
@@ -17,3 +16,14 @@ scot_data <- la_data %>%
   select(Council.Name, Level, Area.Name, Year, Sex, Age, Population)
 
 projection_data <- rbind(projection_data, scot_data)
+rm(la_data, scot_data)
+
+# Add Persons data to projection_data
+persons_data <- projection_data %>%
+  group_by(Council.Name, Level, Area.Name, Year, Age) %>%
+  summarise(Population = sum(Population)) %>%
+  mutate(Sex = "Persons") %>%
+  select(Council.Name, Level, Area.Name, Year, Sex, Age, Population)
+
+projection_data <- rbind(projection_data, persons_data)
+rm(persons_data)
