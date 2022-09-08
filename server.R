4476d9213de584502a158165644eeb6445662674
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-09-08 17:32:12 +0100

    Created function for adding line graphs. Still need to add validation so that the graph only appears once measures are selected but will update once UI Inputs are merged

--- server.R
+++ server.R
@@ -5,10 +5,95 @@ server <- function(input, output) {
   # Function to create Scotland level map - function name = create_scot_map
   
   # Function to add total population index to data - function name = add_pop_index
-  
+  # Function to add total population index to data - function name = add_pop_index
   # Function to create line graphs - function name = create_line_plot
+  create_line_plot <- function(dataset, council_selection, small_area_selection, measure_selection){
+    
+    renderPlotly({
+      
+      data <- if(measure_selection == "Total Population") {
+        filter(dataset, Measure == "Population.Index")
+      } else {
+        if(measure_selection == "Dependency Ratio") {
+         filter(dataset, Measure  == "Dependency.Ratio")
+        } else {
+          if(measure_selection == "Sex Ratio") {
+            filter(dataset, Measure  == "Sex.Ratio")
+          }
+        }
+      }
+    
+      Title <- if(measure_selection == "Total Population") {
+        "Total Population Index"
+      } else {
+        measure_selection
+      }
+    
+      data <- data %>% mutate(Line.Colours = "grey") 
+      data$Line.Colours <- if_else(data$Area.Name == council_selection, 
+                                   "skyblue",
+                                   if_else(data$Area.Name == "Scotland",
+                                           "dimgrey",
+                                           if_else(data$Area.Name == small_area_selection,
+                                                   "steelblue",
+                                                   "grey"
+                                                   )
+                                           )
+                                   )
+    
+      all_area_names <- unique(data$Area.Name)
+      area_factors <- if(length(all_area_names) > 3) {
+        area_factors <- data %>% filter(Area.Name != small_area_selection) 
+        area_factors <- c(small_area_selection, unique(area_factors$Area.Name))
+        } else {
+          area_factors <- c(small_area_selection, council_selection, "Scotland")
+        }
+    
+      data$Area.Name <- factor(data$Area.Name, levels = area_factors)
+      data <- data %>% arrange(Area.Name)
+      area_colours <- data %>% 
+        group_by(Council.Name, Level, Area.Name) %>% 
+        filter(row_number()== 1) 
+      area_colours <- area_colours$Line.Colours
+    
+      plot <- ggplot(data = data) +
+        geom_line(
+          aes(
+          x = Year, 
+          y = Value, 
+          group = Area.Name, 
+          colour = Area.Name,
+          text = paste("Area Name:",`Area.Name`, "<br>", 
+                       "Year:", `Year`,"<br>",
+                       "Measure:", `Title`, "<br>", 
+                       "Value:",`Value`)
+          ), 
+          size = 0.7
+        ) +
+        scale_color_manual(values = area_colours) +
+        labs(title = Title) +
+        theme(
+          plot.title = element_text(size = 11), 
+          legend.title = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(), 
+          panel.background = element_blank(), 
+          axis.line = element_line(colour = "black"),
+          axis.text.x = element_text(vjust = 0.3, angle = 20),
+          axis.text.y = element_text(size = 7),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank()
+        ) +
+        scale_x_continuous(breaks = 2018:2030)
+      ggplotly(plot, tooltip = c("text")) %>% 
+        config(displayModeBar = F) %>% 
+        layout(xaxis = list(fixedrange = TRUE)) %>% 
+        layout(yaxis = list(fixedrange = TRUE)) %>%
+        layout(legend = list(x = 0, y = 1))
+    })
   
-  
+  }
+
 # Reactive expressions (and UI output) for input selections -------------------------------------------
   
   # Reactive expression to store selection from la_choice_tab_1 - variable name = selected_la_tab_1
