f2f9bc1ad2eda183aa9a089d4ac4e3b92fb2202a
Cara Connachan <cara.connachan@improvementservice.org.uk>
2022-09-15 13:24:23 +0100

    Added return() to function and removed dataset argument and replaced with default global data

--- server.R
+++ server.R
@@ -5,9 +5,9 @@ server <- function(input, output) {
   # Function to create Scotland level map - function name = create_scot_map
   
   # Function to add total population index to data - function name = add_pop_index
-  add_pop_index <- function(dataset, gender_selection, age_selection) {
+  add_pop_index <- function(gender_selection, age_selection) {
     
-    total_pop_data <- dataset %>% 
+    total_pop_data <- projection_data %>% 
       filter(Sex == gender_selection & Age %in% age_selection) %>%
       group_by(Council.Name, Level, Area.Name, Year, Sex) %>%
       summarise(Total.Population = sum(Population)) %>%
@@ -42,6 +42,8 @@ server <- function(input, output) {
     data <- merge(total_pop_data, index_data) %>%
       merge(dependency_data) %>% 
       merge(sex_data)
+    
+    return(data)
   }
   
   # Function to create line graphs - function name = create_line_plot
