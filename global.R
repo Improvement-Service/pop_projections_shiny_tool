a1a219b5203082962f93c1879bd6e7a03babcfec
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2022-09-20 14:16:26 +0100

    Add RColorBrewer

--- global.R
+++ global.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(readxl)
 library(leaflet)
 library(plotly)
+library(RColorBrewer)
 
 projection_data <- read.csv("Data files/Population Projections With Aggregations.csv")
 shape_data <- read_rds("Data files/SCAP_shapefile.rds")
