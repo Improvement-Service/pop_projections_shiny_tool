903213e09fcf25e6494c167ebc4d1c1cca5df858
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2022-09-22 16:32:53 +0100

    Change read.csv to read_csv for speed improvement

--- global.R
+++ global.R
@@ -4,7 +4,7 @@ library(readxl)
 library(leaflet)
 library(plotly)
 
-projection_data <- read.csv("Data files/Population Projections With Aggregations.csv")
+projection_data <- read_csv("Data files/Population Projections With Aggregations.csv")
 shape_data <- read_rds("Data files/SCAP_shapefile.rds")
 la_shape_data <- read_rds("Data files/LAShps.rds") 
 
