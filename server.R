f1ce13b513f0110ab6999d428baccf478446cb32
Mairi-Louise MacDonald <mairi_louise@yahoo.co.uk>
2022-09-23 16:08:44 +0100

    two observe events: 1. update small area reactiveVal when LA is selected (with default small area) 2. update small area reactiveVal with specific small area clicked on map

--- server.R
+++ server.R
@@ -163,6 +163,10 @@ server <- function(input, output) {
   })  
   
   # Reactive expression to store default small area selection - variable name = selected_small_area_tab_1
+    #this will be initialised when the use selects an LA and will be updated
+    #either when a new LA is selected or the user clicks on the map
+  selected_small_area_tab_1 <- reactiveVal()
+  
 
   # Reactive expression to store selection from year_choice_tab_1 - variable name = selected_year_tab_1
   selected_year_tab_1 <- reactive({
@@ -206,6 +210,7 @@ server <- function(input, output) {
                   options = list(placeholder = 'Select Area',
                                  onInitialize = I('function() { this.setValue(""); }')
                   )
+    )
   })
   
   # Reactive expression to store selection from small_area_output_tab_2 - variable name = selected_small_area_tab_2
@@ -302,7 +307,23 @@ server <- function(input, output) {
   })
   
   # Create observe event to update selected_small_area_tab_1
+  observe({
+    event <- input$scot_map_tab_1_shape_click
+    if(is.null(event)){
+      return()} 
+    selected_small_area_tab_1(event$id)
+  })
   
+  observe({
+    event <- input$la_choice_tab_1
+    if(is.null(event)){
+      return()} 
+    small_area_options <- small_area_lookup %>%
+      filter(Council.Name == input$la_choice_tab_1) %>%
+      pull(Area.Name)
+    default_area <- small_area_options[1]
+    selected_small_area_tab_1(default_area)
+  })
   # Filter data for across areas graph - variable name = across_areas_data_tab_1
   
   # Run create_line_plot - outputID = across_areas_plot_tab_1
