7529afc084f832d5927ca1a24cd9f2244325d5b0
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2022-09-26 15:05:29 +0100

    Split shapefile names for merging

--- global.R
+++ global.R
@@ -17,8 +17,12 @@ small_area_lookup <- projection_data %>%
   select(Council.Name, Area.Name) %>%
   distinct()
 
-#replace "and" with ampersand in shapefiles
+#replace "and" with ampersand in shapefiles - this is to allow merging with projection_data
 shape_data$`Sub-Council Area Name` <- gsub(" and ", " & ", shape_data$`Sub-Council Area Name`)
 
 #load shortname lookups
 lookup <- read_csv("Data files/ShortNameLookup.csv")
+
+#split out council and sub-council in shape_data for merging
+shape_data[shape_data$`Sub-Council Area Name` == "Annbank Mossblown & Tarbolton - the Coalfields - South Ayrshire", "Sub-Council Area Name"] <- "Annbank Mossblown & Tarbolton: the Coalfields - South Ayrshire"
+shape_data <- shape_data %>% separate(`Sub-Council Area Name`, into = c("SubCouncil", "Council"), sep = " - ", remove = FALSE)
