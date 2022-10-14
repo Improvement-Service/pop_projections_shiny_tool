d8af8af9691cc2a557620849eeefa4eb4f3a2124
caraconnachan <cara.connachan@improvementservice.org.uk>
2022-10-14 13:37:39 +0100

    Formatting fixed to match style guide

--- server.R
+++ server.R
@@ -285,7 +285,9 @@ server <- function(input, output) {
   # Filter data for across areas graph - variable name = across_areas_data_tab_1
   across_areas_data_tab_1 <- reactive({
 #run function to add index to data then filter to selected Council and small area
-    across_data <- add_pop_index(gender_selection= selected_gender_tab_1(), age_selection= input$age_choice_tab_1) %>%
+    across_data <- add_pop_index(gender_selection = selected_gender_tab_1(), 
+                                 age_selection = input$age_choice_tab_1
+                                 ) %>%
       filter(Council.Name %in% c(input$la_choice_tab_1, "Scotland") && 
                Area.Name %in% c(input$la_choice_tab_1, "Scotland", selected_small_area_tab_1())
                )
